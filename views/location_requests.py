cf1e14d965cef2fa4f3a169cdc4a394897666939
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-10-05 16:14:44 -0400

    added database of animals and locations

---
+++ views/location_requests.py
@@ -0,0 +1,12 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North",
+        "address": "8422 Johnson Pike"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South",
+        "address": "209 Emory Drive"
+    }
+]
